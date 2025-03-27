4e3204e6c66a1d01ffb5f0275820c28f443e2b1f
nahyer <reyhanluyai@gmail.com>
2025-03-26 23:42:06 -0700

    changes

--- src/index.ts
+++ src/index.ts
@@ -43,7 +43,7 @@ app.route("/api/auth", authRouter);
 
 
 
-console.log(`Server is running on port ${process.env.PORT}`);
+console.log(`Server is running on http://localhost:${process.env.PORT}`);
 
 
 serve({
